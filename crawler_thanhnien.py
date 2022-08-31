b186589d2ae667df417f7453dc0b455b60f85ea1
Thanh Do <thanhdn@irtech.com.vn>
2022-08-31 12:08:28 +0700

    INIT DATA

---
+++ crawler_thanhnien.py
@@ -0,0 +1,58 @@
+import time
+
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+import json
+
+
+
+def dumpContentJson(my_data, name):
+    jsonString = json.dumps(my_data)
+    jsonFile = open(name + ".json", "w")
+    jsonFile.write(jsonString)
+    jsonFile.close()
+
+def getListPage(min, max):
+    cmsLinks = []
+    cmsRange = list(range(min, max))
+    urlList = []
+    baseUrl = "https://thanhnien.vn/cong-nghe-game/tin-tuc/?trang="
+    for page_num in cmsRange:
+        urlList.append(baseUrl + str(page_num))
+    for url in urlList:
+        data = requests.get(url)
+        html = BeautifulSoup(data.text, 'html.parser')
+        articles = html.select("article", {"class": "story"})
+        for article in articles:
+            articleLink = article.find("a", {"class": "story__title cms-link"})
+            cmsLinks.append(articleLink.get_attribute_list("href")[0])
+    return cmsLinks
+
+
+def getPageContent(cmsLinks):
+    output = []
+    for url in cmsLinks:
+        data = requests.get(url)
+        html = BeautifulSoup(data.text, 'html.parser')
+        body = html.find("div", {"id": "abody"})
+
+        if body is None:
+            continue
+        body = body.find_all("p")
+        title = html.find("h1", {"class": "details__headline cms-title"}).text
+        content = ""
+        for p in body:
+            content += p.getText()
+        output.append({
+            "url": url,
+            "title": title,
+            "content": content
+        })
+    return output
+
+cmsLinks = getListPage(1, 100)
+content = getPageContent(cmsLinks)
+print("Got " + str(len(content)) + "item")
+
+dumpContentJson(content, "cms_content")
